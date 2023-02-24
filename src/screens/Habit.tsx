688ae3e43d14079e463dd2be2148d5d02f9eda70
Diego Ramires <goncalves.ramires@gmail.com>
2023-02-24 11:55:52 -0300

    feat: fetch API day info and store data on state

--- src/screens/Habit.tsx
+++ src/screens/Habit.tsx
@@ -1,22 +1,64 @@
+import { useEffect, useState } from "react";
+import { Alert, ScrollView, Text, View } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import dayjs from "dayjs";
 
-import { ScrollView, Text, View } from "react-native";
+import { api } from "../lib/axios";
+
 import { BackButton } from "../components/BackButton";
 import { ProgressBar } from "../components/ProgressBar";
 import { CheckBox } from "../components/CheckBox";
+import { Loading } from "../components/Loading";
 
 interface Params {
   date: string;
 }
 
+interface DayInfoProps {
+  completedHabits: string[];
+  possibleHabits: {
+    id: string;
+    title: string;
+  }[];
+}
+
 export function Habit() {
+  const [loading, setLoading] = useState(true);
+  const [dayInfo, setDayInfo] = useState<DayInfoProps | null>(null);
+
   const route = useRoute();
   const { date } = route.params as Params;
 
   const parsedDate = dayjs(date);
   const dayOfWeek = parsedDate.format('dddd');
   const dayAndMonth = parsedDate.format('DD/MM');
+
+  async function fetchHabits() {
+    try {
+      setLoading(true);
+
+      const response = await api.get('/day', {
+        params: { date: date }
+      })
+      setDayInfo(response.data);
+
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Ops! 😢', 'Não foi possível carregar os hábitos 🤦')
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchHabits()
+  }, [])
+
+  if(loading) {
+    return (
+      <Loading />
+    )
+  }
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
